cc1f0af912c0e955734e4060305aadaa5b009611
Abishekarjunraj <abishek2003arjunraj@gmail.com>
2024-10-10 09:37:29 +0530

    add verilog file

---
+++ mux_using_buffer.v
@@ -0,0 +1,40 @@
+module mux_using_buffer( out, in1,in2,s );
+	 output out;
+	 input in1,in2,s;
+	 
+bufif1(out,in1,s);
+bufif0(out,in2,s);
+endmodule 
+
+module mux_using_buffer_tb;
+reg in1,in2,s;
+wire out;
+ mux_using_buffer uut(
+          .out(out),
+			 .in1(in1),
+			 .in2(in2),
+			 .s(s) 
+			 ); 
+			 
+initial 
+begin
+$monitor(" #time =%t, in1 =%b,in2=%b, s=%b,out=%b", $time,in1,in2,s,out);
+in1 = 1'b0; in2 = 1'b0; s=1'b0;
+#10;
+in1 = 1'b0; in2 = 1'b0; s=1'b1;
+#10;
+in1 = 1'b0; in2 = 1'b1; s=1'b0;
+#10;
+in1 = 1'b0; in2 = 1'b1; s=1'b1;
+#10;
+in1 = 1'b1; in2 = 1'b0; s=1'b0;
+#10;
+in1 = 1'b1; in2 = 1'b0; s=1'b1;
+#10;
+in1 = 1'b1; in2 = 1'b1; s=1'b0;
+#10;
+in1 = 1'b1; in2 = 1'b1; s=1'b1;
+#10;
+end 
+endmodule
+
